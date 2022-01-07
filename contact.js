9d5a6b10fd6a881eb21b70edb81873490a434adf
avocy <avocyl@gmail.com>
2022-01-07 17:09:33 +0700

    Update v1.5.0

--- contact.js
+++ contact.js
@@ -1,18 +1,23 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw2szkJ2qYvzC9b_TNx4PtHGe91PI27ejfJe62WIwwqLnIvaGrpFKzsCTa0Vj57mw6N/exec';
-      const form = document.forms['wpu-contact-form'];
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-gHIs1Rv6nUTWDwE--0UGB1CmQI98VvIdyxUzb6tCUyVuV0emcqc3CbPBOsZKUng1cg/exec';
+      const form = document.forms['contact'];
       const btnKirim = document.querySelector('.btn-kirim');
       const btnLoading = document.querySelector('.btn-loading');
       const myAlert = document.querySelector('.my-alert');
 
       form.addEventListener('submit', (e) => {
         e.preventDefault();
+        // ketika tombol submit diklik
+        // tampilkan tombol loading, hilangkan tombol kirim
         btnLoading.classList.toggle('d-none');
         btnKirim.classList.toggle('d-none');
         fetch(scriptURL, { method: 'POST', body: new FormData(form) })
           .then((response) => {
+            // tampilkan tombol kirim, hilangkan tombol loading
             btnLoading.classList.toggle('d-none');
             btnKirim.classList.toggle('d-none');
+            // tampilkan alert
             myAlert.classList.toggle('d-none');
+            // reset formnya
             form.reset();
             console.log('Success!', response);
           })
